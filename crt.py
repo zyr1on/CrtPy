6703d6058148168bde8a3dcd66e036fb674e1609
zyr1on <66450395+zyr1on@users.noreply.github.com>
2020-06-30 20:47:00 +0300

    Update crt.py

--- crt.py
+++ crt.py
@@ -9,7 +9,7 @@ if len(sys.argv) < 2:
 	print("usage python3 crt.py <domain>")
 else:
 	domain = sys.argv[1]
-	print("Enumerating Domains >> " , domain , "\n")
+	print("Enumerating >> " , domain , "\n")
 	base_url = (f"https://crt.sh/?q={domain}&output=json")
 	req = requests.get(base_url).content
 	#req = requests.get(base_url).content.decode('utf-8')

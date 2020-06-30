86b3566c6cbf934da5739cd05225ed6eed49a884
zyr1on <66450395+zyr1on@users.noreply.github.com>
2020-06-30 20:42:22 +0300

    Update crt.py

--- crt.py
+++ crt.py
@@ -5,16 +5,14 @@ import requests
 import json
 import sys
 
-if len(sys.argv) < 2:
+if len(sys.argv) <= 2:
 	print("usage python3 crt.py <domain>")
 else:
-	pass
-	
-domain = sys.argv[1]
-print("Enumerating Domains >> " , domain , "\n")
-base_url = (f"https://crt.sh/?q={domain}&output=json")
-req = requests.get(base_url).content
-#req = requests.get(base_url).content.decode('utf-8')
-data = json.loads(req)
-for keys,values in enumerate(data):
-	print(values['name_value'])
+	domain = sys.argv[1]
+	print("Enumerating Domains >> " , domain , "\n")
+	base_url = (f"https://crt.sh/?q={domain}&output=json")
+	req = requests.get(base_url).content
+	#req = requests.get(base_url).content.decode('utf-8')
+	data = json.loads(req)
+	for keys,values in enumerate(data):
+		print(values['name_value'])

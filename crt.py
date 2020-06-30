2c79e2c5f2c247137f45d3b0f02110f9f6588db8
zyr1on <66450395+zyr1on@users.noreply.github.com>
2020-06-30 20:25:32 +0300

    Update crt.py

--- crt.py
+++ crt.py
@@ -14,6 +14,7 @@ domain = sys.argv[1]
 print("Enumerating Domains >> " , domain , "\n")
 base_url = (f"https://crt.sh/?q={domain}&output=json")
 req = requests.get(base_url).content
+#req = requests.get(base_url).content.decode('utf-8')
 data = json.loads(req)
 for keys,values in enumerate(data):
 	print(values['name_value'])

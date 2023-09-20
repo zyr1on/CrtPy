3149873e351a8507d4bdf673dac807e42a1baf3b
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2023-09-21 02:50:50 +0300

    Update crt_requests.py

--- crt_requests.py
+++ crt_requests.py
@@ -9,24 +9,38 @@ parser.add_argument("--domain","-d",required=True,help="specify the target domai
 parser.add_argument("--output","-o",required=False,help="specify output file name")
 args = parser.parse_args()
 output = str(args.output)
+domain = args.domain
+print("Enumerating >>>",domain )
 
 if int(sys.version_info.major) < 3:
     print("Run With Python3")
     exit()
-else:
-    domain = args.domain
-    print("Enumerating >> " , domain , "\n")
-    url = (f"https://crt.sh/?q={domain}&output=json")
+
+url = (f"https://crt.sh/?q={domain}&output=json")
+
+try:
     data_req = requests.get(url).content
     data = json.loads(data_req)
-    for a,b in enumerate(data):
-        domains = str(b['name_value'])
-        print(domains)
-        if output == None:
-            pass
-        elif output == "None":
-            pass
-        else:
-            file = open(output,"a")
+    if output == None or output == 'None':
+        for a , b in enumerate(data):
+            domains = str(b['name_value'])
+            print(domains)
+    else:
+        file = open(output,"a")
+        counter = 0
+        for a,b in enumerate(data):
+            domains = str(b['name_value'])
             file.write(domains)
-            file.close()
+        file.close()
+except KeyboardInterrupt:
+        print("\nExiting...")
+except Exception as err:
+        print(err)
+except requests.exceptions.RequestException as err:
+    print ("Request Exception:",err)
+except requests.exceptions.HTTPError as errh:
+    print ("Http Error:",errh)
+except requests.exceptions.ConnectionError as errc:
+    print ("Error Connecting:",errc)
+except requests.exceptions.Timeout as errt:
+    print ("Timeout Error:",errt)     
